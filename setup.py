ebf76f0c7e35f2e82a2c0e8ba7dcad254be57755
adhrit2019 <adhrit2019@gmail.com>
2021-07-29 19:41:59 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setup(name='pymeanings',
+      version='1.0',
+      description='Getting meanings in a GUI dailog box',
+      long_description=readme()
+      url='http://github.com/storborg/funniest',
+      author='Adhrit Pramanik',
+      author_email='adhrit2019@gmail.com',
+      license='MIT',
+      packages=['pymeanings'],
+      install_packages=['PyDictionary', 'tkinter', 'subprocess'],
+      zip_safe=False,
+      entry_points={
+                    'console_scripts':['pymeaning=pymeanings.command_line:main']
+      })
