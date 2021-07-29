1e03992144d2564662f7b567fde8905f134623cf
adhrit2019 <adhrit2019@gmail.com>
2021-07-29 21:07:39 +0530

    change version to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pymeanings',
-      version='1.0',
+      version='0.1',
       description='Getting meanings in a GUI dailog box',
       long_description=readme(),
       url='https://github.com/adhrit2019/pymeanings',
@@ -13,7 +13,7 @@ setup(name='pymeanings',
       author_email='adhrit2019@gmail.com',
       license='MIT',
       packages=['pymeanings'],
-      install_requires=['PyDictionary', 'tkinter', 'subprocess'],
+      install_requires=['PyDictionary'],
       zip_safe=False,
       entry_points={
                     'console_scripts':['pymeaning=pymeanings.command_line:main']
